1c63b5343aeb7599a3a22e6d54344738188de6f5
Rob Sokolowski <rpsoko@gmail.com>
2021-09-05 04:38:18 -0400

    very happy with this

--- src/Components/Styling.elm
+++ src/Components/Styling.elm
@@ -19,9 +19,21 @@ red =
     Element.rgb 0.8 0 0
 
 
+black =
+    Element.rgb 0 0 0
+
+
 darkBlue =
     Element.rgb 0 0 0.8
 
 
 softGrey =
     Element.rgb255 211 211 211
+
+
+medGrey =
+    Element.rgb255 177 177 177
+
+
+dimGrey =
+    Element.rgb255 105 105 105
