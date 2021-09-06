9530c6f8bce7900fd7b2cadaadd08d166581962e
Rob Sokolowski <rpsoko@gmail.com>
2021-09-06 19:46:50 -0400

    add component harness, not sure if this is a good idea..

---
+++ src/Dev/ComponentDemoData.elm
@@ -0,0 +1,69 @@
+module Dev.ComponentDemoData exposing (..)
+
+import Api.Card exposing (CardEnvelope, FlashCard(..), PromptFrequency(..))
+import Time
+
+
+catalogTableDemoData : List CardEnvelope
+catalogTableDemoData =
+    [ { id = 1
+      , card =
+            Markdown
+                { question = ""
+                , renderedQuestion = ""
+                , answer = ""
+                , renderedAnswer = ""
+                , tags = []
+                }
+      , userId = -999999
+      , createdAt = Time.millisToPosix 1630971219000
+      , lastModifiedOn = Time.millisToPosix 1630971219000
+      , nextPromptSchedFor = Time.millisToPosix 1630971219000
+      , frequency = Immediately
+      }
+    , { id = 2
+      , card =
+            Markdown
+                { question = ""
+                , renderedQuestion = ""
+                , answer = ""
+                , renderedAnswer = ""
+                , tags = []
+                }
+      , userId = -999999
+      , createdAt = Time.millisToPosix 1630971219000
+      , lastModifiedOn = Time.millisToPosix 1630971219000
+      , nextPromptSchedFor = Time.millisToPosix 1630971219000
+      , frequency = Immediately
+      }
+    , { id = 3
+      , card =
+            Markdown
+                { question = ""
+                , renderedQuestion = ""
+                , answer = ""
+                , renderedAnswer = ""
+                , tags = []
+                }
+      , userId = -999999
+      , createdAt = Time.millisToPosix 1630971219000
+      , lastModifiedOn = Time.millisToPosix 1630971219000
+      , nextPromptSchedFor = Time.millisToPosix 1630971219000
+      , frequency = Immediately
+      }
+    , { id = 4
+      , card =
+            Markdown
+                { question = ""
+                , renderedQuestion = ""
+                , answer = ""
+                , renderedAnswer = ""
+                , tags = []
+                }
+      , userId = -999999
+      , createdAt = Time.millisToPosix 1630971219000
+      , lastModifiedOn = Time.millisToPosix 1630971219000
+      , nextPromptSchedFor = Time.millisToPosix 1630971219000
+      , frequency = Immediately
+      }
+    ]
