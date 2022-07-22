79d18d818463b604b0cc618d1f798f22c514e0aa
Rob Sokolowski <rpsoko@gmail.com>
2022-07-22 09:19:53 -0400

    shoddy impl of evergreen migration

--- src/Evergreen/Migrate/V13.elm
+++ src/Evergreen/Migrate/V13.elm
@@ -7,7 +7,7 @@ import Lamdera.Migrations exposing (..)
 
 frontendModel : Old.FrontendModel -> ModelMigration New.FrontendModel New.FrontendMsg
 frontendModel old =
-    Unimplemented
+    ModelUnchanged
 
 
 backendModel : Old.BackendModel -> ModelMigration New.BackendModel New.BackendMsg
@@ -17,7 +17,7 @@ backendModel old =
 
 frontendMsg : Old.FrontendMsg -> MsgMigration New.FrontendMsg New.FrontendMsg
 frontendMsg old =
-    Unimplemented
+    MsgUnchanged
 
 
 toBackend : Old.ToBackend -> MsgMigration New.ToBackend New.BackendMsg
@@ -32,4 +32,4 @@ backendMsg old =
 
 toFrontend : Old.ToFrontend -> MsgMigration New.ToFrontend New.FrontendMsg
 toFrontend old =
-    Unimplemented
+    MsgUnchanged
