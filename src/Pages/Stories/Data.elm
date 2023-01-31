a740bca2b41d9ec62345a62b2b5d7ba0ef4a7f18
Rob Sokolowski <rpsoko@gmail.com>
2023-01-30 22:28:02 -0500

    update demo

---
+++ src/Pages/Stories/Data.elm
@@ -0,0 +1,16 @@
+module Pages.Stories.Data exposing (initialText)
+
+
+initialText =
+    """
+Pythagoras sez: $a^2 + b^2 = c^2$.  Extremely cool!
+
+This will be on the test:
+
+$$
+\\int_0^1 x^n dx = \\frac{1}{n+ 1}
+$$
+
+Study hard!
+
+"""
