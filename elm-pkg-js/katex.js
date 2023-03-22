30b774f037d5dfc9181cd199b45cfe7e81b21094
Rob Sokolowski <rpsoko@gmail.com>
2023-03-22 05:59:28 -0400

    remove old attemps, some cleanup, katex demo story basic

---
+++ elm-pkg-js/katex.js
@@ -0,0 +1,34 @@
+exports.init = async (app) => {
+    var katexJs = document.createElement('script')
+    katexJs.type = 'text/javascript'
+    katexJs.onload = initKatex
+    katexJs.src = "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js"
+
+    document.head.appendChild(katexJs);
+}
+
+initKatex = () => {
+    console.log("elm-katex: initializing");
+
+    class MathText extends HTMLElement {
+        constructor() {
+            super();
+        }
+
+        connectedCallback() {
+            this.attachShadow({mode: "open"});
+            this.shadowRoot.innerHTML =
+                katex.renderToString(
+                    this.content,
+                    { throwOnError: false, displayMode: this.display }
+                );
+            let link = document.createElement('link');
+            link.setAttribute('rel', 'stylesheet');
+            link.setAttribute('href', 'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css');
+            this.shadowRoot.appendChild(link);
+
+        }
+    }
+
+    customElements.define('math-text', MathText);
+}
